7c7d3ac28a3e96d4a7efde4505b86403702ea8fd
Rafael Sergio <rafa.sergio.02@live.com>
2020-03-25 14:39:30 -0300

    criado pagina de logon

---
+++ src/pages/Logon/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { FiLogIn } from "react-icons/fi";
+import "./style.css";
+
+import logoImg from "../../assets/logo.svg";
+import heroesImg from "../../assets/heroes.png";
+
+export default function Logon() {
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The hero" />
+        <form>
+          <h1>faça seu logo</h1>
+          <input placeholder="sua ID" />
+          <button className="button" type="submit">
+            Entrar
+          </button>
+          <a href="/register">
+            <FiLogIn size={16} color="#e02041" />
+            Não tenho cadastro
+          </a>
+        </form>
+      </section>
+      <img src={heroesImg} alt="Heroes" />
+    </div>
+  );
+}
