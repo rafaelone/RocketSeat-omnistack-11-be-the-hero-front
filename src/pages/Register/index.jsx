762479baec82496f925d1cac964b73290b7bdb6b
Rafael Sergio <rafa.sergio.02@live.com>
2020-03-25 18:06:33 -0300

    criada pagina de registro

---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,34 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import { FiArrowLeft } from "react-icons/fi"
+import "./style.css"
+
+import logoImg from "../../assets/logo.svg"
+
+export default function Register(){
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The hero"/>
+          <h1>Cadastro</h1>
+          <p>Faça seu cadastro, entre na plataforma e ajude pessoa a encontrarem os casos da sua ONG.</p>
+          <Link to="/" className="back-link">
+            <FiArrowLeft size={16} color="#e02041" />
+            Não tenho cadastro
+          </Link>
+        </section>
+        <form >
+          <input placeholder="Nome da ONG"/>
+          <input type="email" placeholder="E-mail"/>
+          <input placeholder="WhatsApp"/>
+          <div className="input-group">
+            <input placeholder="cidade"/>
+            <input placeholder="UF" style={{ width: 80 }}/>
+          </div>
+          <button className="button" type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  )
+}
